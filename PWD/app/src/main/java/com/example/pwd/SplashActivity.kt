05d9ed4e5041001560c2dbc49ed4101e4ed0f071
patrick0422 <didxodnd0422@gmail.com>
2021-03-04 21:18:31 +0900

    프로젝트 생성
    ㅇㅇ

---
+++ PWD/app/src/main/java/com/example/pwd/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.pwd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        var handler : Handler
+
+    }
+}
