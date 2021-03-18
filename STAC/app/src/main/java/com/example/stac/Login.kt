aebd3608607fbb61bac792c0dbfd5de81cc9a2c5
ZzukMa <67806878+minhyuuk@users.noreply.github.com>
2021-03-18 15:01:24 +0900

    back to latest version

--- STAC/app/src/main/java/com/example/stac/Login.kt
+++ STAC/app/src/main/java/com/example/stac/Login.kt
@@ -1,11 +1,8 @@
 package com.example.stac
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import android.widget.EditText
 import android.widget.Toast
 import com.example.stac.databinding.ActivityLoginBinding
 
