10fb318e2a1b0cd4aebc2c32ded8070beb8f3671
Minhyuk <jlssteeve1@gmail.com>
2021-08-20 15:50:31 +0900

    #1-splash-screen: 스플래시 화면

--- welfaretree/app/src/main/java/net/welfaretree/welfaretree/SplashActivity.kt
+++ welfaretree/app/src/main/java/net/welfaretree/welfaretree/SplashActivity.kt
@@ -3,12 +3,46 @@ package net.welfaretree.welfaretree
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(applicationContext, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.activity_splash)
+
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+
+        motionLayout.addTransitionListener(object :MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
     }
 }
