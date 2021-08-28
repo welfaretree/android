053d8afa25b5562a2bcdcc15b874a99884eeed8f
Minhyuk <jlssteeve1@gmail.com>
2021-08-28 15:17:09 +0900

    #10-user-tutorial: viewHolder -> inner class

--- welfaretree/app/src/main/java/com/example/welfaretree/MyIntroPagerRecyclerAdapter.kt
+++ welfaretree/app/src/main/java/com/example/welfaretree/MyIntroPagerRecyclerAdapter.kt
@@ -1,13 +1,35 @@
 package com.example.welfaretree
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MyIntroPagerRecyclerAdapter(private var pageList:ArrayList<PageItem>): RecyclerView.Adapter<MyPagerViewHolder>(){
+class MyIntroPagerRecyclerAdapter(private var pageList: ArrayList<PageItem>) :
+    RecyclerView.Adapter<MyIntroPagerRecyclerAdapter.MyPagerViewHolder>() {
+
+
+    inner class MyPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val itemImage = itemView.findViewById<ImageView>(R.id.pager_item_image)
+        private val itemTitle = itemView.findViewById<TextView>(R.id.pager_item_text_title)
+        private val itemExplain = itemView.findViewById<TextView>(R.id.pager_item_text_explain)
+
+
+        fun bindWithView(pageItem: PageItem) {
+            itemImage.setImageResource(pageItem.imageSrc)
+            itemTitle.text = pageItem.title
+            itemExplain.text = pageItem.explain
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPagerViewHolder {
-        return MyPagerViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_intro_pager_item, parent, false))
+        return MyPagerViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.layout_intro_pager_item, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
