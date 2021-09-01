4f62be0ff367dfd1ed5ad7032bc1c5d01f75585e
Minhyuk <jlssteeve1@gmail.com>
2021-09-01 13:57:21 +0900

    #21-main-tool-bar: 툴바 구현 (아직 미완)

--- welfaretree/app/src/main/java/com/example/welfaretree/activity/MainActivity.kt
+++ welfaretree/app/src/main/java/com/example/welfaretree/activity/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.welfaretree.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.example.welfaretree.R
 import com.example.welfaretree.databinding.ActivityMainBinding
 import com.example.welfaretree.fragment.*
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -20,8 +23,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        bottomNavigation()
+        supportActionBar()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tool_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item!!.itemId) {
+            R.id.action_search -> {
+                Snackbar.make(findViewById(R.id.toolbar), "search pressed", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+            R.id.action_share -> {
+                Snackbar.make(findViewById(R.id.toolbar),"share pressed",Snackbar.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun bottomNavigation() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.bottomNavigationView.setOnItemSelectedListener {
             val fragmentTransaction = fragmentManager.beginTransaction()
             when (it.itemId) {
@@ -30,19 +58,23 @@ class MainActivity : AppCompatActivity() {
                     fragmentTransaction.addToBackStack(null)
                 }
                 R.id.menu_calendar -> {
-                    fragmentTransaction.replace(R.id.pager, calendarFragment).commitAllowingStateLoss()
+                    fragmentTransaction.replace(R.id.pager, calendarFragment)
+                        .commitAllowingStateLoss()
                     fragmentTransaction.addToBackStack(null)
                 }
                 R.id.menu_profile -> {
-                    fragmentTransaction.replace(R.id.pager, profileFragment).commitAllowingStateLoss()
+                    fragmentTransaction.replace(R.id.pager, profileFragment)
+                        .commitAllowingStateLoss()
                     fragmentTransaction.addToBackStack(null)
                 }
                 R.id.menu_notification -> {
-                    fragmentTransaction.replace(R.id.pager, notificationFragment).commitAllowingStateLoss()
+                    fragmentTransaction.replace(R.id.pager, notificationFragment)
+                        .commitAllowingStateLoss()
                     fragmentTransaction.addToBackStack(null)
                 }
                 R.id.menu_setting -> {
-                    fragmentTransaction.replace(R.id.pager, settingFragment).commitAllowingStateLoss()
+                    fragmentTransaction.replace(R.id.pager, settingFragment)
+                        .commitAllowingStateLoss()
                     fragmentTransaction.addToBackStack(null)
                 }
             }
@@ -51,4 +83,10 @@ class MainActivity : AppCompatActivity() {
 
         fragmentManager.beginTransaction().add(R.id.pager, HomeFragment()).commitAllowingStateLoss()
     }
+
+    private fun supportActionBar() {
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar!!.setDisplayShowTitleEnabled(true) // 타이틀 보이게 설정
+    }
+
 }
