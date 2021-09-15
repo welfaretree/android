7c9aa257d3aa03907fdd365889cc6216213aca0c
sunb <devsunb@gmail.com>
2021-09-15 16:46:36 +0900

    #1-welfare-calendar: 복지 날짜 화면 구현 (API 통신 제외)

--- welfaretree/app/src/main/java/com/example/welfaretree/fragment/CalendarFragment.kt
+++ welfaretree/app/src/main/java/com/example/welfaretree/fragment/CalendarFragment.kt
@@ -1,18 +1,48 @@
 package com.example.welfaretree.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.welfaretree.R
+import java.util.*
+
 
 class CalendarFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+        val calendar: CalendarView = view.findViewById(R.id.calendar)
+        val calendarListTitle: TextView = view.findViewById(R.id.calendar_list_title)
+        val calendarListContent: TextView = view.findViewById(R.id.calendar_list_content)
+
+        val today = Calendar.getInstance()
+        val todayYear = today.get(Calendar.YEAR)
+        val todayMonth = today.get(Calendar.MONTH)
+        val todayDayOfMonth = today.get(Calendar.DAY_OF_MONTH)
+
+        calendarListTitle.text =
+            "${padDate(todayYear)}.${padDate(todayMonth)}.${padDate(todayDayOfMonth)}"
+        calendarListContent.text = getWelfare(todayYear, todayMonth, todayDayOfMonth)
+
+        calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            calendarListTitle.text = "${padDate(year)}.${padDate(month)}.${padDate(dayOfMonth)}"
+            calendarListContent.text = getWelfare(year, month, dayOfMonth)
+        }
+        return view
+    }
+
+    private fun padDate(i: Int): String {
+        return i.toString().padStart(2, '0')
+    }
+
+    private fun getWelfare(year: Int, month: Int, dayOfMonth: Int): String {
+        // TODO: 일자 별 복지 목록 API 접근 코드 구현 필요
+        return "- 복지 목록"
     }
 }
