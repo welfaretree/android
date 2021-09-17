1c0b87c8a3ca3c34721b208e03c35d587a187b24
양태웅 <didxodnd0422@gmail.com>
2021-09-17 18:48:00 +0900

    #27-user-profile: 유저 프로필 xml 추가

---
+++ welfaretree/app/src/main/java/com/stac/welfaretree/adapter/ProfileAdapter.kt
@@ -0,0 +1,23 @@
+package com.stac.welfaretree.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProfileAdapter(): RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
