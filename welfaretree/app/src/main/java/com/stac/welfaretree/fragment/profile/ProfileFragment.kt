e49bcaa2cec5a399e56cf109b2035ac92bb71cc3
Taewoong Yang <didxodnd0422@gmail.com>
2021-09-22 19:52:39 +0900

    #27-user-profile: 인적사항 삭제 시 다이얼로그 표시

--- welfaretree/app/src/main/java/com/stac/welfaretree/fragment/profile/ProfileFragment.kt
+++ welfaretree/app/src/main/java/com/stac/welfaretree/fragment/profile/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.stac.welfaretree.fragment.profile
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.welfaretree.R
 import com.example.welfaretree.databinding.FragmentProfileBinding
 import com.stac.welfaretree.adapter.ProfileAdapter
 
@@ -49,5 +52,17 @@ class ProfileFragment : Fragment() {
 
     fun editUserInfo() {
         Toast.makeText(activity, "인적사항 편집", Toast.LENGTH_SHORT).show()
+    }
+    fun deleteUserInfo() {
+        AlertDialog.Builder(activity)
+            .setTitle("정말 인적사항을 삭제하시겠습니까?")
+            .setMessage("삭제시 등록된 모든 복지가 사라집니다.")
+            .setPositiveButton(R.string.alert_positive_message) { _, _ ->
+                _deleteUserInfo()
+            }
+            .show()
+    }
+    private fun _deleteUserInfo() {
+        Toast.makeText(activity, "인적사항이 삭제되었습니다.", Toast.LENGTH_SHORT).show()
     }
 }
