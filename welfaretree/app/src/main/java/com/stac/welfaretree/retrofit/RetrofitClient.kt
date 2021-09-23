19bb70a9a12f29e379cffc687482a1696df1b331
정민혁 <jlssteeve1@gmail.com>
2021-09-23 10:51:56 +0900

    feature/#9-search-screen: client 생성

---
+++ welfaretree/app/src/main/java/com/stac/welfaretree/retrofit/RetrofitClient.kt
@@ -0,0 +1,67 @@
+package com.stac.welfaretree.retrofit
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import javax.security.cert.X509Certificate
+
+// single
+object RetrofitClient {
+
+    // declare
+    private var retrofitClient: Retrofit? = null
+
+    fun getClient(baseUrl:String): Retrofit? {
+        Log.d(TAG,"RetrofitClient - getClient() called")
+
+        // create instance
+        if(retrofitClient == null){
+            retrofitClient = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(getUnsafeOkHttpClient().build())
+                .build()
+        }
+
+        return retrofitClient
+    }
+    
+    private fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(
+                chain: Array<out java.security.cert.X509Certificate>?,
+                authType: String?
+            ) {
+
+            }
+
+            override fun checkServerTrusted(
+                chain: Array<out java.security.cert.X509Certificate>?,
+                authType: String?
+            ) {
+
+            }
+
+            override fun getAcceptedIssuers(): Array<out java.security.cert.X509Certificate>? {
+                return arrayOf()
+            }
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        val sslSocketFactory = sslContext.socketFactory
+
+        val builder = OkHttpClient.Builder()
+        builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        builder.hostnameVerifier { hostname, session -> true }
+
+        return builder
+    }
+}
